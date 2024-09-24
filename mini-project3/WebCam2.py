38cfdbb8c4896b9dcefaf975fd667778cc1cf376
Qinhao Zhang <lbjason0802@gmail.com>
2024-09-23 22:26:03 -0700

    draft by Jason

--- mini-project3/WebCam2.py
+++ mini-project3/WebCam2.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # Initialize the camera
 cap = cv2.VideoCapture(0)  # Change the index if you have multiple cameras
@@ -20,8 +21,42 @@ while True:
     # Display the resulting frame
     cv2.imshow('Original and Grayscale', combined)
 
+    # Get the height and width of the frame
+    h, w = frame.shape[:2]
+
+    # Translation - shift the img horizontally and vertically
+    tx, ty = 50, 30
+    translation_matrix = np.float32([[1, 0, tx], [0, 1, ty]])
+    translated_frame = cv2.warpAffine(frame, translation_matrix, (w, h))
+
+    # Rotation
+    center = (w // w, h // 2)
+    angle = 45
+    rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1)
+    rotated_img = cv2.warpAffine(frame, rotation_matrix, (h, w))
+
+    # Scaling 
+    scale_size = 0.5
+    scaled_img = cv2.resize(frame, None, fx = scale_size, fy = scale_size)
+
+    # Perspective Transformation
+    p1 = np.float32([[0,0], [w - 1, 0], [0, h - 1], [w - 1, h - 1]])
+    p2 = np.float32([[0,0], [w - 1, 0], [w * 0.2, h - 1], [w * 0.8, h - 1]])
+    perspective_matrix = cv2.getPerspectiveTransform(p1, p2)
+    perspective_frame = cv2.warpPerspective(frame, perspective_matrix, (w, h))
+
+
     # Break the loop on 'q' key press
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    key = cv2.waitKey(1)
+    if key == ord('t'):
+        cv2.imshow('translation', translated_frame)
+    if key == ord('r'):
+        cv2.imshow('rotated', rotated_img)
+    if key == ord('s'):
+        cv2.imshow('scaled', scaled_img)
+    if key == ord('p'):
+        cv2.imshow('perspective', perspective_frame)
+    if key == ord('q') & 0xFF:
         break
 
 # Release the camera and close all OpenCV windows
